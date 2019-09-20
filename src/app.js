1319e80bad15cc3dcdd5b59ba21a18d2a6b8dcde
Elise De Smet <elise.desmet99@gmail.com>
2019-09-20 14:17:17 +0200

    Basic hello world web server

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+const http = require('http'); //http=package
+const server = http.createServer(); //server aanmaken en laten luisteren op poort 3000
+
+server.on('request', (request, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write('Hello world');
+    response.end(); //connectie afsluiten
+});
+
+server.listen(3000, () => {
+    console.log('Node server created at port 3000');
+});
