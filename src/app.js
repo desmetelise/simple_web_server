96e5e27d24dec1a26f5841b2d5eb05a7f2e77423
Elise De Smet <elise.desmet99@gmail.com>
2019-09-20 14:38:31 +0200

    Add routing

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
 const http = require('http'); //http=package
 const server = http.createServer(); //server aanmaken en laten luisteren op poort 3000
+const url = require('url');
+
+function set_response(response, content, status = 200) {
+    response.writeHead(status, { 'Content-Type': 'text/plain' });
+    response.write(content);
+}
 
 server.on('request', (request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write('Hello world');
+    let path = url.parse(request.url).pathname;//url van request meegeven en parsen en pathname geven (alles achter/)
+    console.log(path);
+
+    if (path === '/') {//"": enscape chars herkennen, ==: boolean, ===
+        set_response(response, 'Hello World');
+    } else if (path === '/about') {
+        set_response(response, 'Made by VIVES students. ;)');
+    } else {
+        set_response(response, 'Error', 400);
+    }
     response.end(); //connectie afsluiten
 });
 
